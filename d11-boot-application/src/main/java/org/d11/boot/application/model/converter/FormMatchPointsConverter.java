67d5565ecd1a3f3b02f0cffa3ff35faf862f4a2d
dromelvan <dromelvan@aland.net>
2021-06-06 10:54:28 +0300

    FormMatchPointsConverter handles empty form match points list.

--- d11-boot-application/src/main/java/org/d11/boot/application/model/converter/FormMatchPointsConverter.java
+++ d11-boot-application/src/main/java/org/d11/boot/application/model/converter/FormMatchPointsConverter.java
@@ -1,5 +1,8 @@
 package org.d11.boot.application.model.converter;
 
+
+import org.apache.commons.lang3.StringUtils;
+
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.util.ArrayList;
@@ -19,19 +22,23 @@ public class FormMatchPointsConverter implements AttributeConverter<List<Integer
     @Override
     public String convertToDatabaseColumn(final List<Integer> integers) {
         final StringBuilder stringBuilder = new StringBuilder();
-        for(final Integer integer : integers) {
-            stringBuilder.append(integer);
-            stringBuilder.append(DELIMITER);
+        if(!integers.isEmpty()) {
+            for(final Integer integer : integers) {
+                stringBuilder.append(integer);
+                stringBuilder.append(DELIMITER);
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         }
-        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         return stringBuilder.toString();
     }
 
     @Override
     public List<Integer> convertToEntityAttribute(final String string) {
         final List<Integer> integers = new ArrayList<>();
-        for(final String integer : string.split(DELIMITER)) {
-            integers.add(Integer.parseInt(integer));
+        if(!StringUtils.isEmpty(string)) {
+            for(final String integer : string.split(DELIMITER)) {
+                integers.add(Integer.parseInt(integer));
+            }
         }
         return integers;
     }
