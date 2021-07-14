6ee9a7a7078d0d515e171548d8579df78dfb1574
dromelvan <dromelvan@aland.net>
2021-07-14 09:04:34 +0300

    Simplified Jasypt decryption.

--- d11-boot-application/src/main/java/org/d11/boot/application/D11BootApplication.java
+++ d11-boot-application/src/main/java/org/d11/boot/application/D11BootApplication.java
@@ -1,11 +1,7 @@
 package org.d11.boot.application;
 
-import org.d11.boot.application.security.EncryptionConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.Console;
-import java.util.Arrays;
 
 /**
  * Main class of the D11 Boot application.
@@ -20,22 +16,12 @@ public class D11BootApplication {
     public static final long VERSION = 1L;
 
     /**
-     * Starts the D11 Boot application. Encryption password is provided by console or the
-     * -Dencryption.configuration.password=password system property.
+     * Starts the D11 Boot application.
      *
      * @param args Application parameters. Not used.
      */
     public static void main(final String[] args) {
-        final Console console = System.console();
-        // Console is not available if we're running in IDE or Gradle bootRun.
-        if (console == null) {
-            SpringApplication.run(D11BootApplication.class, args);
-        } else {
-            final String key = new String(console.readPassword("Secret Encryption Key: "));
-            String[] enrichedArgs = Arrays.copyOf(args, args.length + 1);
-            enrichedArgs[enrichedArgs.length - 1] = String.format("--%s=%s", EncryptionConfiguration.ENCRYPTION_PASSWORD, key);
-            SpringApplication.run(D11BootApplication.class, enrichedArgs);
-        }
+        SpringApplication.run(D11BootApplication.class, args);
     }
 
 }
