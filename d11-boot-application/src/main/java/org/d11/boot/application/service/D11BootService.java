c0fb804be14529c60d2e2f6474c78a03afd18f76
dromelvan <dromelvan@aland.net>
2021-08-06 16:44:30 +0300

    Service method for validating entities.

--- d11-boot-application/src/main/java/org/d11/boot/application/service/D11BootService.java
+++ d11-boot-application/src/main/java/org/d11/boot/application/service/D11BootService.java
@@ -1,10 +1,32 @@
 package org.d11.boot.application.service;
 
+import org.apache.commons.lang3.StringUtils;
+import org.d11.boot.application.model.D11Entity;
 import org.d11.boot.application.util.MappingProvider;
+
+import javax.validation.ConstraintViolation;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Base class for services.
  */
 public class D11BootService extends MappingProvider {
+
+    /**
+     * Validates provided entities.
+     *
+     * @param entities The entities that will be provided.
+     * @return List of validation error messages.
+     */
+    protected List<String> getValidationErrors(final D11Entity... entities) {
+        final List<String> errors = new ArrayList<>();
+        for(final D11Entity entity : entities) {
+            for(ConstraintViolation<D11Entity> constraintViolation : entity.validate()) {
+                errors.add(StringUtils.capitalize(constraintViolation.getPropertyPath().toString()) + " " + constraintViolation.getMessage());
+            }
+        }
+        return errors;
+    }
 
 }
