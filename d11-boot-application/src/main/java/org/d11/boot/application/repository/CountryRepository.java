b04bec16bfbcf7c843b87c2b453bc0bcdcb9954f
dromelvan <dromelvan@aland.net>
2021-10-30 13:12:32 +0300

    Find by name method in Country repository.

--- d11-boot-application/src/main/java/org/d11/boot/application/repository/CountryRepository.java
+++ d11-boot-application/src/main/java/org/d11/boot/application/repository/CountryRepository.java
@@ -1,15 +1,25 @@
 package org.d11.boot.application.repository;
 
 import org.d11.boot.application.model.Country;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Repository for country entities.
  */
 @Repository
 public interface CountryRepository extends D11EntityRepository<Country> {
+
+    /**
+     * Finds a country with a specific name.
+     *
+     * @param name Name of the country that will be looked up.
+     * @return Country with the specified name.
+     */
+    Optional<Country> findByName(@Param("name") String name);
 
     /**
      * Finds all countries.
