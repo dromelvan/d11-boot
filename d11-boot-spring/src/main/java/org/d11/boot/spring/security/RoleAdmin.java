b5e84cf42925a4de83053c97cd8f39b9de92d744
dromelvan <dromelvan@aland.net>
2023-09-17 01:10:44 +0300

    Added controller method role authorization annotations.

---
+++ d11-boot-spring/src/main/java/org/d11/boot/spring/security/RoleAdmin.java
@@ -0,0 +1,18 @@
+package org.d11.boot.spring.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Pre authorises a method by requiring the current user has the ADMIN role.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+public @interface RoleAdmin {
+
+}
