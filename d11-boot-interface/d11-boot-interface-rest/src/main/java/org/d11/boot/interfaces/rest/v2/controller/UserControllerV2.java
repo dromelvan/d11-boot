746ea4274173a67b86a225fcf77a2806d012bf89
dromelvan <dromelvan@aland.net>
2023-10-04 21:37:13 +0300

    Added update user API operation.

--- d11-boot-interface/d11-boot-interface-rest/src/main/java/org/d11/boot/interfaces/rest/v2/controller/UserControllerV2.java
+++ d11-boot-interface/d11-boot-interface-rest/src/main/java/org/d11/boot/interfaces/rest/v2/controller/UserControllerV2.java
@@ -2,11 +2,13 @@ package org.d11.boot.interfaces.rest.v2.controller;
 
 import org.d11.boot.api.v2.UserApi;
 import org.d11.boot.api.v2.model.CreateUserRequestBodyDTO;
+import org.d11.boot.api.v2.model.UpdateUserRequestBodyDTO;
 import org.d11.boot.api.v2.model.UserDTO;
 import org.d11.boot.api.v2.model.UserResponseBodyDTO;
 import org.d11.boot.interfaces.rest.RepositoryServiceController;
 import org.d11.boot.spring.model.User;
 import org.d11.boot.spring.model.UserRegistration;
+import org.d11.boot.spring.security.RoleUser;
 import org.d11.boot.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -36,6 +38,19 @@ public class UserControllerV2 extends RepositoryServiceController<UserService> i
         final UserResponseBodyDTO userResponseBodyDTO = new UserResponseBodyDTO()
                 .user(map(user, UserDTO.class));
         return new ResponseEntity<>(userResponseBodyDTO, HttpStatus.CREATED);
+    }
+
+    @Override
+    @RoleUser
+    public ResponseEntity<UserResponseBodyDTO> updateUser(final Long userId,
+                                                          final UpdateUserRequestBodyDTO updateUserRequestBodyDTO) {
+        final User user = getRepositoryService().updateUserPassword(userId,
+                                                                    updateUserRequestBodyDTO.getCurrentPassword(),
+                                                                    updateUserRequestBodyDTO.getPassword(),
+                                                                    updateUserRequestBodyDTO.getConfirmPassword());
+        final UserResponseBodyDTO userResponseBodyDTO = new UserResponseBodyDTO()
+                .user(map(user, UserDTO.class));
+        return ResponseEntity.ok(userResponseBodyDTO);
     }
 
 }
--- d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/UserControllerV2Tests.java
+++ d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/UserControllerV2Tests.java
@@ -3,6 +3,7 @@ package org.d11.boot.interfaces.rest.v2.controller;
 import feign.FeignException;
 import org.d11.boot.api.v2.client.UserApi;
 import org.d11.boot.api.v2.model.CreateUserRequestBodyDTO;
+import org.d11.boot.api.v2.model.UpdateUserRequestBodyDTO;
 import org.d11.boot.api.v2.model.UserResponseBodyDTO;
 import org.d11.boot.spring.model.User;
 import org.d11.boot.spring.repository.UserRepository;
@@ -20,6 +21,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * User controller tests.
  */
 class UserControllerV2Tests extends D11BootControllerV2Tests {
+
+    /**
+     * Password.
+     */
+    private static final String PASSWORD = "password";
 
     /**
      * User repository.
@@ -42,13 +48,12 @@ class UserControllerV2Tests extends D11BootControllerV2Tests {
 
         final String name = "name";
         final String email = "email@email.com";
-        final String password = "password";
         final String confirmPassword = "confirmPassword";
 
         final CreateUserRequestBodyDTO createUserRequestBodyDTO = new CreateUserRequestBodyDTO()
                 .email(email)
-                .password(password)
-                .confirmPassword(password);
+                .password(PASSWORD)
+                .confirmPassword(PASSWORD);
 
         assertThrows(FeignException.BadRequest.class, () -> userApi.createUser(createUserRequestBodyDTO),
                      "UserController::createUser name missing throws");
@@ -65,7 +70,7 @@ class UserControllerV2Tests extends D11BootControllerV2Tests {
         assertThrows(FeignException.BadRequest.class, () -> userApi.createUser(createUserRequestBodyDTO),
                      "UserController::createUser password missing throws");
 
-        createUserRequestBodyDTO.password(password).confirmPassword(null);
+        createUserRequestBodyDTO.password(PASSWORD).confirmPassword(null);
         assertThrows(FeignException.BadRequest.class, () -> userApi.createUser(createUserRequestBodyDTO),
                      "UserController::createUser confirmPassword missing throws");
 
@@ -73,7 +78,7 @@ class UserControllerV2Tests extends D11BootControllerV2Tests {
         assertThrows(FeignException.BadRequest.class, () -> userApi.createUser(createUserRequestBodyDTO),
                      "UserController::createUser password/confirmPassword don't match throws");
 
-        createUserRequestBodyDTO.confirmPassword(password);
+        createUserRequestBodyDTO.confirmPassword(PASSWORD);
 
         final User existingUser = this.userRepository.findAll().get(0);
 
@@ -94,9 +99,104 @@ class UserControllerV2Tests extends D11BootControllerV2Tests {
 
         assertEquals(name, user.getName(), "UserController::createUser user name equals");
         assertEquals(email, user.getEmail(), "UserController::createUser user email equals");
-        assertTrue(this.passwordEncoder.matches(password, user.getEncryptedPassword()),
+        assertTrue(this.passwordEncoder.matches(PASSWORD, user.getEncryptedPassword()),
                    "UserController::createUser user password matches");
         assertFalse(user.isAdministrator(), "UserController::createUser user isAdministrator");
+    }
+
+    /**
+     * Tests UserController::updateUser.
+     */
+    @Test
+    void testUpdateUser() {
+        final User user = this.userRepository.findByName("User").orElseThrow(NotFoundException::new);
+        final User admin = this.userRepository.findByName("Administrator").orElseThrow(NotFoundException::new);
+
+        final UpdateUserRequestBodyDTO updateUserRequestBodyDTO = new UpdateUserRequestBodyDTO()
+                .currentPassword(PASSWORD)
+                .password(PASSWORD)
+                .confirmPassword(PASSWORD);
+
+        // Unauthorized ------------------------------------------------------------------------------------------------
+
+        final UserApi unauthorizedApi = getApi(UserApi.class);
+
+        assertThrows(FeignException.Unauthorized.class,
+                     () -> unauthorizedApi.updateUser(admin.getId(), updateUserRequestBodyDTO),
+                     "UserController::updateUser unauthorized throws");
+
+        // Bad Request -------------------------------------------------------------------------------------------------
+
+        final UserApi userApi = getUserApi(UserApi.class);
+
+        updateUserRequestBodyDTO.setCurrentPassword(null);
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> userApi.updateUser(user.getId(), updateUserRequestBodyDTO),
+                     "UserController::updateUser currentPassword missing throws");
+
+        updateUserRequestBodyDTO.setCurrentPassword(PASSWORD);
+        updateUserRequestBodyDTO.setPassword(null);
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> userApi.updateUser(user.getId(), updateUserRequestBodyDTO),
+                     "UserController::updateUser password missing throws");
+
+        updateUserRequestBodyDTO.setPassword(PASSWORD);
+        updateUserRequestBodyDTO.setConfirmPassword(null);
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> userApi.updateUser(user.getId(), updateUserRequestBodyDTO),
+                     "UserController::updateUser confirmPassword missing throws");
+
+        updateUserRequestBodyDTO.setConfirmPassword(user.getEncryptedPassword());
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> userApi.updateUser(user.getId(), updateUserRequestBodyDTO),
+                     "UserController::updateUser password mismatch throws");
+
+        updateUserRequestBodyDTO.setConfirmPassword(PASSWORD);
+
+        // Forbidden ---------------------------------------------------------------------------------------------------
+
+        assertThrows(FeignException.Forbidden.class,
+                     () -> userApi.updateUser(admin.getId(), updateUserRequestBodyDTO),
+                     "UserController::updateUser not current user throws");
+
+        updateUserRequestBodyDTO.currentPassword("INVALID");
+
+        assertThrows(FeignException.Forbidden.class,
+                     () -> userApi.updateUser(user.getId(), updateUserRequestBodyDTO),
+                     "UserController::updateUser invalid current password throws");
+
+        updateUserRequestBodyDTO.currentPassword(PASSWORD);
+
+        // Not Found ---------------------------------------------------------------------------------------------------
+
+        assertThrows(FeignException.NotFound.class,
+                     () -> unauthorizedApi.updateUser(null, updateUserRequestBodyDTO),
+                     "UserController::updateUser not found throws");
+
+        // OK ----------------------------------------------------------------------------------------------------------
+
+        final String newPassword = "newPassword";
+
+        updateUserRequestBodyDTO
+                .password(newPassword)
+                .confirmPassword(newPassword);
+
+        final UserResponseBodyDTO userResponseBodyDTO = userApi.updateUser(user.getId(), updateUserRequestBodyDTO);
+        final User result = this.userRepository.findById(userResponseBodyDTO.getUser().getId())
+                .orElseThrow(NotFoundException::new);
+
+        assertEquals(user.getId(), result.getId(), "UserController::updateUser result id equals");
+        assertTrue(this.passwordEncoder.matches(newPassword, result.getEncryptedPassword()),
+                   "UserController::updateUser new password matches");
+
+        // Rollback ----------------------------------------------------------------------------------------------------
+
+        result.setEncryptedPassword(this.passwordEncoder.encode(PASSWORD));
+        this.userRepository.save(result);
     }
 
 }
