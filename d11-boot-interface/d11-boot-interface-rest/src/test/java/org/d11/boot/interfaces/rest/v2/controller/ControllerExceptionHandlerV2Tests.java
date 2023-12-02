7324768af6c23f238483069e919b4980c3e081f1
dromelvan <dromelvan@aland.net>
2023-12-02 22:30:06 +0200

    Added tests for unauthorized controller exception handling.

--- d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/ControllerExceptionHandlerV2Tests.java
+++ d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/ControllerExceptionHandlerV2Tests.java
@@ -3,12 +3,14 @@ package org.d11.boot.interfaces.rest.v2.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.d11.boot.api.v2.model.BadRequestResponseBodyDTO;
 import org.d11.boot.util.exception.BadRequestException;
+import org.d11.boot.util.exception.UnauthorizedException;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -42,9 +44,14 @@ class ControllerExceptionHandlerV2Tests {
 
         final ResponseEntity<BadRequestResponseBodyDTO> responseEntity =
                 controllerExceptionHandlerV2.handle(e, httpServletRequest);
+
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.BAD_REQUEST,
+                     "ControllerExceptionHandlerV2::handle(BadRequestException) status equals");
+
         final BadRequestResponseBodyDTO badRequestResponseBodyDTO = responseEntity.getBody();
 
-        assertNotNull(badRequestResponseBodyDTO, "ControllerExceptionHandlerV2::handle(BadRequestException) not null");
+        assertNotNull(badRequestResponseBodyDTO,
+                      "ControllerExceptionHandlerV2::handle(BadRequestException) body not null");
         assertNotNull(badRequestResponseBodyDTO.getTimestamp(),
                       "ControllerExceptionHandlerV2::handle(BadRequestException) timestamp not null");
         assertEquals(HttpStatus.BAD_REQUEST.getReasonPhrase(), badRequestResponseBodyDTO.getError(),
@@ -60,6 +67,26 @@ class ControllerExceptionHandlerV2Tests {
                      "ControllerExceptionHandlerV2::handle(BadRequestException) validationError property equals");
         assertEquals(e.getMessage(), badRequestResponseBodyDTO.getValidationErrors().get(0).getError(),
                      "ControllerExceptionHandlerV2::handle(BadRequestException) validationError error equals");
+    }
+
+    /**
+     * Tests ControllerExceptionHandlerV2::handle(UnauthorizedException).
+     */
+    @Test
+    void testHandleUnauthorizedException() {
+        final UnauthorizedException e = new UnauthorizedException();
+
+        final HttpServletRequest httpServletRequest = mock(HttpServletRequest.class);
+        when(httpServletRequest.getRequestURI()).thenReturn(REQUEST_URI);
+        when(httpServletRequest.getMethod()).thenReturn(METHOD);
+
+        final ControllerExceptionHandlerV2 controllerExceptionHandlerV2 = new ControllerExceptionHandlerV2();
+
+        final ResponseEntity<?> responseEntity = controllerExceptionHandlerV2.handle(e, httpServletRequest);
+
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.UNAUTHORIZED,
+                     "ControllerExceptionHandlerV2::handle(UnauthorizedException) status equals");
+        assertNull(responseEntity.getBody(), "ControllerExceptionHandlerV2::handle(UnauthorizedException) body null");
     }
 
 }
