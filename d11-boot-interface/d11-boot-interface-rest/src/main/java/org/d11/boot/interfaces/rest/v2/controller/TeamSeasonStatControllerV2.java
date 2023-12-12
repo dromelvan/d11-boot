f57602ceccd22adf657fbfc320b52baf7e6cf7de
dromelvan <dromelvan@aland.net>
2023-12-12 17:10:24 +0200

    Added get team season stats by season id API operation.

---
+++ d11-boot-interface/d11-boot-interface-rest/src/main/java/org/d11/boot/interfaces/rest/v2/controller/TeamSeasonStatControllerV2.java
@@ -0,0 +1,40 @@
+package org.d11.boot.interfaces.rest.v2.controller;
+
+import org.d11.boot.api.v2.TeamSeasonStatApi;
+import org.d11.boot.api.v2.model.TeamSeasonStatDTO;
+import org.d11.boot.api.v2.model.TeamSeasonStatsResponseBodyDTO;
+import org.d11.boot.interfaces.rest.RepositoryServiceController;
+import org.d11.boot.spring.model.TeamSeasonStat;
+import org.d11.boot.spring.service.TeamSeasonStatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Team season stat API REST controller implementation.
+ */
+@RestController
+public class TeamSeasonStatControllerV2 extends RepositoryServiceController<TeamSeasonStatService>
+        implements TeamSeasonStatApi {
+
+    /**
+     * Create a new controller.
+     *
+     * @param teamSeasonStatService The service the controller will use.
+     */
+    @Autowired
+    public TeamSeasonStatControllerV2(final TeamSeasonStatService teamSeasonStatService) {
+        super(teamSeasonStatService);
+    }
+
+    @Override
+    public ResponseEntity<TeamSeasonStatsResponseBodyDTO> getTeamSeasonStatsBySeasonId(final Long seasonId) {
+        final List<TeamSeasonStat> teamSeasonStats = getRepositoryService().getBySeasonId(seasonId);
+
+        return ResponseEntity.ok(new TeamSeasonStatsResponseBodyDTO()
+                                         .teamSeasonStats(getMapper().map(teamSeasonStats, TeamSeasonStatDTO.class)));
+    }
+
+}
---
+++ d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/TeamSeasonStatControllerV2Tests.java
@@ -0,0 +1,77 @@
+package org.d11.boot.interfaces.rest.v2.controller;
+
+import feign.FeignException;
+import org.d11.boot.api.v2.client.TeamSeasonStatApi;
+import org.d11.boot.api.v2.model.TeamSeasonStatDTO;
+import org.d11.boot.api.v2.model.TeamSeasonStatsResponseBodyDTO;
+import org.d11.boot.spring.model.Season;
+import org.d11.boot.spring.model.TeamSeasonStat;
+import org.d11.boot.spring.repository.TeamSeasonStatRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Team season stat controller tests.
+ */
+class TeamSeasonStatControllerV2Tests extends D11BootControllerV2Tests {
+
+    /**
+     * Team season stat repository.
+     */
+    @Autowired
+    private TeamSeasonStatRepository teamSeasonStatRepository;
+
+    /**
+     * Tests TeamSeasonStatController::getTeamSeasonStatsBySeasonId.
+     */
+    @Test
+    void testGetD11TeamSeasonStatsBySeasonId() {
+        final TeamSeasonStatApi teamSeasonStatApi = getApi(TeamSeasonStatApi.class);
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> teamSeasonStatApi.getTeamSeasonStatsBySeasonId((Long) null),
+                     "TeamSeasonStatController::getTeamSeasonStatsBySeasonId seasonId null throws");
+
+        final List<TeamSeasonStat> teamSeasonStats = this.teamSeasonStatRepository.findAll();
+        teamSeasonStats.sort(Comparator.comparing(TeamSeasonStat::getRanking));
+
+        final Set<Season> seasons = teamSeasonStats.stream()
+                .map(TeamSeasonStat::getSeason)
+                .collect(Collectors.toSet());
+
+        assertTrue(seasons.size() > 1, "TeamSeasonStatController::getTeamSeasonStatsBySeasonId seasons size > 0");
+
+        for (final Season season : seasons) {
+            final TeamSeasonStatsResponseBodyDTO teamSeasonStatsBySeasonId =
+                    teamSeasonStatApi.getTeamSeasonStatsBySeasonId(season.getId());
+            assertNotNull(teamSeasonStatsBySeasonId,
+                          "TeamSeasonStatController::getTeamSeasonStatsBySeasonId response not null");
+
+            final List<TeamSeasonStat> expected = teamSeasonStats.stream()
+                    .filter(teamSeasonStat -> teamSeasonStat.getSeason().equals(season))
+                    .toList();
+
+            assertTrue(expected.size() > 1,
+                       "TeamSeasonStatController::getTeamSeasonStatsBySeasonId expected size > 1");
+
+            final List<TeamSeasonStatDTO> result = teamSeasonStatsBySeasonId.getTeamSeasonStats();
+
+            assertNotNull(result, "TeamSeasonStatController::getTeamSeasonStatsBySeasonId not null ");
+            assertFalse(result.isEmpty(), "TeamSeasonStatController::getTeamSeasonStatsBySeasonId empty");
+            assertEquals(map(expected, TeamSeasonStatDTO.class), result,
+                         "TeamSeasonStatController::getTeamSeasonStatsBySeasonId equals");
+        }
+    }
+
+}
