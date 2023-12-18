fbb2e91db4a64a61b627bd97de81cacb703fbeef
dromelvan <dromelvan@aland.net>
2023-12-18 22:27:18 +0200

    Added get player season stats by season id API operation tests.

---
+++ d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/PlayerSeasonStatControllerV2Tests.java
@@ -0,0 +1,89 @@
+package org.d11.boot.interfaces.rest.v2.controller;
+
+import feign.FeignException;
+import org.d11.boot.api.v2.client.PlayerSeasonStatApi;
+import org.d11.boot.api.v2.model.PlayerSeasonStatDTO;
+import org.d11.boot.api.v2.model.PlayerSeasonStatsResponseBodyDTO;
+import org.d11.boot.spring.model.PlayerSeasonStat;
+import org.d11.boot.spring.model.Season;
+import org.d11.boot.spring.repository.PlayerSeasonStatRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Player season stat controller tests.
+ */
+class PlayerSeasonStatControllerV2Tests extends D11BootControllerV2Tests {
+
+    /**
+     * Player season stat repository.
+     */
+    @Autowired
+    private PlayerSeasonStatRepository playerSeasonStatRepository;
+
+    /**
+     * Tests PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId.
+     */
+    @Test
+    void testGetD11TeamSeasonStatsBySeasonId() {
+        final PlayerSeasonStatApi playerSeasonStatApi = getApi(PlayerSeasonStatApi.class);
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> playerSeasonStatApi.getPlayerSeasonStatsBySeasonId(null, 0),
+                     "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId seasonId null throws");
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> playerSeasonStatApi.getPlayerSeasonStatsBySeasonId(-1L, 0),
+                     "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId seasonId negative throws");
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> playerSeasonStatApi.getPlayerSeasonStatsBySeasonId(1L, null),
+                     "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId page null throws");
+
+        assertThrows(FeignException.BadRequest.class,
+                     () -> playerSeasonStatApi.getPlayerSeasonStatsBySeasonId(1L, -1),
+                     "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId page negative throws");
+
+        final List<PlayerSeasonStat> playerSeasonStats = this.playerSeasonStatRepository.findAll();
+        playerSeasonStats.sort(Comparator.comparing(PlayerSeasonStat::getRanking));
+
+        final Set<Season> seasons = playerSeasonStats.stream()
+                .map(PlayerSeasonStat::getSeason)
+                .collect(Collectors.toSet());
+
+        assertTrue(seasons.size() > 1, "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId seasons size > 0");
+
+        for (final Season season : seasons) {
+            final PlayerSeasonStatsResponseBodyDTO playerSeasonStatsBySeasonId =
+                    playerSeasonStatApi.getPlayerSeasonStatsBySeasonId(season.getId(), 0);
+            assertNotNull(playerSeasonStatsBySeasonId,
+                          "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId response not null");
+
+            final List<PlayerSeasonStat> expected = playerSeasonStats.stream()
+                    .filter(playerSeasonStat -> playerSeasonStat.getSeason().equals(season))
+                    .toList();
+
+            assertTrue(expected.size() > 1,
+                       "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId expected size > 1");
+
+            final List<PlayerSeasonStatDTO> result = playerSeasonStatsBySeasonId.getPlayerSeasonStats();
+
+            assertNotNull(result, "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId not null ");
+            assertFalse(result.isEmpty(), "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId empty");
+            assertEquals(map(expected, PlayerSeasonStatDTO.class), result,
+                         "PlayerSeasonStatController::getPlayerSeasonStatsBySeasonId equals");
+        }
+    }
+
+}
