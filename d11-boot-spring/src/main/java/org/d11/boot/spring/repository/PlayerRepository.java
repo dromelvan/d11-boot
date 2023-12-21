74b2118b56ddf8d98ca3629574fa4205017135c6
dromelvan <dromelvan@aland.net>
2023-12-21 22:30:04 +0200

    Added player repository find by parameterized name like method.

--- d11-boot-spring/src/main/java/org/d11/boot/spring/repository/PlayerRepository.java
+++ d11-boot-spring/src/main/java/org/d11/boot/spring/repository/PlayerRepository.java
@@ -1,6 +1,8 @@
 package org.d11.boot.spring.repository;
 
 import org.d11.boot.spring.model.Player;
+import org.d11.boot.spring.model.PlayerSearchResult;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -36,5 +38,32 @@ public interface PlayerRepository extends D11EntityRepository<Player> {
      * @return List of players with the parameterized name.
      */
     List<Player> findByParameterizedName(@Param("parameterizedName") String parameterizedName);
+
+    /**
+     * Finds players with parameterized names that match the provided parameterized name.
+     *
+     * @param parameterizedName The parameterized name that will be matched to existing parameterized names.
+     * @return List of player search results with matching parameterized names.
+     */
+    // There's no UNION in JPQL, so we have to make a native query for this.
+    @Query(value =
+           """ 
+           SELECT ps.id, TRIM(FROM concat(first_name, ' ', last_name)) AS name, ts.id AS teamId, ts.name AS teamName
+           FROM player ps
+                JOIN player_season_stat psss ON ps.id = psss.player_id
+                JOIN team ts ON ts.id = psss.team_id
+                WHERE season_id = (SELECT MAX(id) FROM season)
+                AND parameterized_name LIKE CONCAT('%', :parameterizedName, '%')
+           UNION
+           SELECT ps.id, TRIM(FROM concat(first_name, ' ', last_name)) AS name, 1 AS teamId, 'None' AS teamName
+           FROM player ps
+                WHERE parameterized_name LIKE CONCAT('%', :parameterizedName, '%')
+                AND NOT EXISTS(SELECT id
+                               FROM player_season_stat psss
+                               WHERE player_id = ps.id AND season_id = (SELECT MAX(id) FROM season))
+           ORDER BY name
+           """,
+           nativeQuery = true)
+    List<PlayerSearchResult> findByParameterizedNameLike(@Param("parameterizedName") String parameterizedName);
 
 }
--- d11-boot-spring/src/test/java/org/d11/boot/spring/repository/PlayerRepositoryTests.java
+++ d11-boot-spring/src/test/java/org/d11/boot/spring/repository/PlayerRepositoryTests.java
@@ -1,13 +1,17 @@
 package org.d11.boot.spring.repository;
 
+import org.apache.commons.lang3.StringUtils;
 import org.d11.boot.spring.model.Player;
+import org.d11.boot.spring.model.PlayerSearchResult;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Player repository tests.
@@ -62,10 +66,83 @@ class PlayerRepositoryTests extends AbstractRepositoryTests<Player, PlayerReposi
             assertFalse(result.isEmpty(), "PlayerRepository::findByParameterizedName empty");
 
             result.forEach(resultPlayer ->
-                                   assertEquals(player.getParameterizedName(),
-                                                resultPlayer.getParameterizedName(),
-                                                "PlayerRepository::findByParameterizedName parameterized name"));
+                    assertEquals(player.getParameterizedName(),
+                                 resultPlayer.getParameterizedName(),
+                                 "PlayerRepository::findByParameterizedName parameterized name"));
         });
+    }
+
+    /**
+     * Tests PlayerRepository::findByParameterizedNameLike.
+     */
+    @Test
+    void testFindByParameterizedNameLike() {
+        // There are players named Bar Foo, Foo and Foo Bar available for this
+        final List<Player> fooPlayers = getRepository().findAll().stream()
+                .filter(player -> player.getParameterizedName().matches(".*foo.*"))
+                .sorted(Comparator.comparing(Player::getName))
+                .toList();
+
+        final List<PlayerSearchResult> fooResult = getRepository().findByParameterizedNameLike("foo");
+
+        assertFalse(fooResult.isEmpty(), "PlayerRepository::findByParameterizedNameLike foo isEmpty");
+        assertEquals(fooPlayers.size(), fooResult.size(),
+                     "PlayerRepository::findByParameterizedNameLike foo size equals");
+
+        for (int i = 0; i < fooPlayers.size(); ++i) {
+            assertEquals(fooPlayers.get(i).getId(), fooResult.get(i).getId(),
+                         "PlayerRepository::findByParameterizedNameLike foo id equals");
+            assertEquals(fooPlayers.get(i).getName(), fooResult.get(i).getName(),
+                         "PlayerRepository::findByParameterizedNameLike foo name equals");
+            assertTrue(fooResult.get(i).getTeamId() > 0,
+                       "PlayerRepository::findByParameterizedNameLike foo teamId greaterThan");
+            assertTrue(StringUtils.isNotBlank(fooResult.get(i).getTeamName()),
+                       "PlayerRepository::findByParameterizedNameLike foo teamName not blank");
+        }
+
+        final List<Player> barPlayers = getRepository().findAll().stream()
+               .filter(player -> player.getParameterizedName().matches(".*bar.*"))
+               .sorted(Comparator.comparing(Player::getName))
+               .toList();
+
+        final List<PlayerSearchResult> barResult = getRepository().findByParameterizedNameLike("bar");
+
+        assertFalse(barResult.isEmpty(), "PlayerRepository::findByParameterizedNameLike bar isEmpty");
+        assertEquals(barPlayers.size(), barResult.size(),
+                     "PlayerRepository::findByParameterizedNameLike bar size equals");
+
+        for (int i = 0; i < barPlayers.size(); ++i) {
+            assertEquals(barPlayers.get(i).getId(), barResult.get(i).getId(),
+                         "PlayerRepository::findByParameterizedNameLike bar id equals");
+            assertEquals(barPlayers.get(i).getName(), barResult.get(i).getName(),
+                         "PlayerRepository::findByParameterizedNameLike bar name equals");
+            assertTrue(barResult.get(i).getTeamId() > 0,
+                       "PlayerRepository::findByParameterizedNameLike bar teamId greaterThan");
+            assertTrue(StringUtils.isNotBlank(barResult.get(i).getTeamName()),
+                       "PlayerRepository::findByParameterizedNameLike bar teamName not blank");
+        }
+
+        final List<Player> fooBarPlayers = getRepository().findAll().stream()
+               .filter(player -> player.getParameterizedName().matches(".*foo-bar.*"))
+               .sorted(Comparator.comparing(Player::getName))
+               .toList();
+
+        final List<PlayerSearchResult> fooBarResult = getRepository().findByParameterizedNameLike("foo-bar");
+
+        assertFalse(fooBarResult.isEmpty(), "PlayerRepository::findByParameterizedNameLike foo-bar size greaterThan");
+        assertEquals(fooBarPlayers.size(), fooBarResult.size(),
+                     "PlayerRepository::findByParameterizedNameLike foo-bar size equals");
+
+        for (int i = 0; i < fooBarPlayers.size(); ++i) {
+            assertEquals(fooBarPlayers.get(i).getId(), fooBarResult.get(i).getId(),
+                         "PlayerRepository::findByParameterizedNameLike foo-bar id equals");
+            assertEquals(fooBarPlayers.get(i).getName(), fooBarResult.get(i).getName(),
+                         "PlayerRepository::findByParameterizedNameLike foo-bar name equals");
+            assertTrue(fooBarResult.get(i).getTeamId() > 0,
+                       "PlayerRepository::findByParameterizedNameLike foo-bar teamId greaterThan");
+            assertTrue(StringUtils.isNotBlank(fooBarResult.get(i).getTeamName()),
+                       "PlayerRepository::findByParameterizedNameLike foo-bar teamName not blank");
+        }
     }
 
 }
