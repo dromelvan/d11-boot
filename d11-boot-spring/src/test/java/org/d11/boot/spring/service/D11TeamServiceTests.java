de34d16af79affb54cd96393113e8a8511f8b85b
dromelvan <dromelvan@aland.net>
2024-01-04 20:55:50 +0200

    Added get D11 team by id service method.

--- d11-boot-spring/src/test/java/org/d11/boot/spring/service/D11TeamServiceTests.java
+++ d11-boot-spring/src/test/java/org/d11/boot/spring/service/D11TeamServiceTests.java
@@ -2,15 +2,20 @@ package org.d11.boot.spring.service;
 
 import org.d11.boot.spring.model.D11Team;
 import org.d11.boot.spring.repository.D11TeamRepository;
+import org.d11.boot.util.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import java.util.List;
+import java.util.Optional;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 /**
@@ -29,6 +34,26 @@ class D11TeamServiceTests extends BaseD11BootServiceTests {
      */
     @InjectMocks
     private D11TeamService d11TeamService;
+
+    /**
+     * Tests D11TeamService::getById.
+     */
+    @Test
+    void testGetById() {
+        final List<D11Team> d11Teams = generateList(D11Team.class);
+        when(this.d11TeamRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        for (final D11Team d11Team : d11Teams) {
+            when(this.d11TeamRepository.findById(d11Team.getId())).thenReturn(Optional.of(d11Team));
+
+            final D11Team result = this.d11TeamService.getById(d11Team.getId());
+            assertNotNull(result, "D11TeamService::getById not null");
+            assertEquals(d11Team, result, "D11TeamService::getById");
+        }
+
+        assertThrows(NotFoundException.class, () -> this.d11TeamService.getById(-1L),
+                     "D11TeamService::getById not found");
+    }
 
     /**
      * Tests D11TeamService::getD11Teams.
