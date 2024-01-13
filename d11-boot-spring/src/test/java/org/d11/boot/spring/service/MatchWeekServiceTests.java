5f1c248e95eb0b1f34c6b4f2c31b8d2e5a2c13f8
dromelvan <dromelvan@aland.net>
2024-01-13 21:47:29 +0200

    Tweaked get current match week service method test.

--- d11-boot-spring/src/test/java/org/d11/boot/spring/service/MatchWeekServiceTests.java
+++ d11-boot-spring/src/test/java/org/d11/boot/spring/service/MatchWeekServiceTests.java
@@ -19,6 +19,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -85,6 +88,9 @@ class MatchWeekServiceTests extends BaseD11BootServiceTests {
 
         assertEquals(pending, this.matchWeekService.getCurrentMatchWeek(),
                      "MatchWeekService::getCurrentMatchWeek pending");
+
+        verify(this.matchWeekRepository, times(3)).findFirstBySeasonStatusOrderByDateAsc(eq(Status.PENDING));
+        verify(this.matchWeekRepository, times(2)).findFirstByDateLessThanEqualOrderByDateDesc(any(LocalDate.class));
     }
 
     /**
