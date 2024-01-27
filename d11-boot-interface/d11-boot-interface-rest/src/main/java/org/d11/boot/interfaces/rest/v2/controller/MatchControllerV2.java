1b747a3cd134e75cdef3fd541023837f2bbc2f91
dromelvan <dromelvan@aland.net>
2024-01-27 22:58:25 +0200

    Added get matches by match week id API operation.

--- d11-boot-interface/d11-boot-interface-rest/src/main/java/org/d11/boot/interfaces/rest/v2/controller/MatchControllerV2.java
+++ d11-boot-interface/d11-boot-interface-rest/src/main/java/org/d11/boot/interfaces/rest/v2/controller/MatchControllerV2.java
@@ -1,8 +1,10 @@
 package org.d11.boot.interfaces.rest.v2.controller;
 
 import org.d11.boot.api.v2.MatchApi;
+import org.d11.boot.api.v2.model.MatchBaseDTO;
 import org.d11.boot.api.v2.model.MatchDTO;
 import org.d11.boot.api.v2.model.MatchResponseBodyDTO;
+import org.d11.boot.api.v2.model.MatchesResponseBodyDTO;
 import org.d11.boot.api.v2.model.StadiumDTO;
 import org.d11.boot.interfaces.rest.RepositoryServiceController;
 import org.d11.boot.spring.model.Match;
@@ -10,6 +12,8 @@ import org.d11.boot.spring.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Match API REST controller implementation.
@@ -34,6 +38,13 @@ public class MatchControllerV2 extends RepositoryServiceController<MatchService>
         return ResponseEntity.ok(new MatchResponseBodyDTO()
                 .match(getMapper().map(match, MatchDTO.class))
                 .stadium(getMapper().map(match.getStadium(), StadiumDTO.class)));
+    }
+
+    @Override
+    public ResponseEntity<MatchesResponseBodyDTO> getMatchesByMatchWeekId(final Long matchWeekId) {
+        final List<Match> matches = getRepositoryService().getByMatchWeekId(matchWeekId);
+        return ResponseEntity.ok(new MatchesResponseBodyDTO()
+                .matches(getMapper().map(matches, MatchBaseDTO.class)));
     }
 
 }
