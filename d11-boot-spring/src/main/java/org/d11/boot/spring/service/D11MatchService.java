c29af482043db2370e0a73d44c7e9e96d9b13696
dromelvan <dromelvan@aland.net>
2024-02-10 20:42:11 +0200

    Added get D11 matches by match week id service method.

--- d11-boot-spring/src/main/java/org/d11/boot/spring/service/D11MatchService.java
+++ d11-boot-spring/src/main/java/org/d11/boot/spring/service/D11MatchService.java
@@ -2,8 +2,11 @@ package org.d11.boot.spring.service;
 
 import org.d11.boot.spring.model.D11Match;
 import org.d11.boot.spring.repository.D11MatchRepository;
+import org.d11.boot.util.exception.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * D11 Match service.
@@ -19,6 +22,20 @@ public class D11MatchService extends RepositoryService<D11Match, D11MatchReposit
     @Autowired
     public D11MatchService(final D11MatchRepository d11MatchRepository) {
         super(D11Match.class, d11MatchRepository);
+    }
+
+    /**
+     * Get D11 matches by match week id ordered by datetime and id.
+     *
+     * @param matchWeekId The match week id.
+     * @return Matches by match week id ordered by datetime and id.
+     */
+    public List<D11Match> getByMatchWeekId(final Long matchWeekId) {
+        if (matchWeekId == null || matchWeekId <= 0) {
+            throw new BadRequestException("matchWeekId", "must be positive");
+        }
+
+        return getJpaRepository().findByMatchWeekIdOrderByDatetimeAscIdAsc(matchWeekId);
     }
 
 }
