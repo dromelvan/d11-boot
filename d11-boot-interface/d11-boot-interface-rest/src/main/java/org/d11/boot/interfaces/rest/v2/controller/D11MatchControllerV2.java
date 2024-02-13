02cefa79e0a2cbd2df9a251534196326101a673f
dromelvan <dromelvan@aland.net>
2024-02-13 22:35:58 +0200

    Added get D11 matches by match week id API operation.

--- d11-boot-interface/d11-boot-interface-rest/src/main/java/org/d11/boot/interfaces/rest/v2/controller/D11MatchControllerV2.java
+++ d11-boot-interface/d11-boot-interface-rest/src/main/java/org/d11/boot/interfaces/rest/v2/controller/D11MatchControllerV2.java
@@ -1,14 +1,18 @@
 package org.d11.boot.interfaces.rest.v2.controller;
 
 import org.d11.boot.api.v2.D11MatchApi;
+import org.d11.boot.api.v2.model.D11MatchBaseDTO;
 import org.d11.boot.api.v2.model.D11MatchDTO;
 import org.d11.boot.api.v2.model.D11MatchResponseBodyDTO;
+import org.d11.boot.api.v2.model.D11MatchesResponseBodyDTO;
 import org.d11.boot.interfaces.rest.RepositoryServiceController;
 import org.d11.boot.spring.model.D11Match;
 import org.d11.boot.spring.service.D11MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * D11 match API REST controller implementation.
@@ -32,6 +36,13 @@ public class D11MatchControllerV2 extends RepositoryServiceController<D11MatchSe
 
         return ResponseEntity.ok(new D11MatchResponseBodyDTO()
                 .d11Match(getMapper().map(d11Match, D11MatchDTO.class)));
+    }
+
+    @Override
+    public ResponseEntity<D11MatchesResponseBodyDTO> getD11MatchesByMatchWeekId(final Long matchWeekId) {
+        final List<D11Match> d11Matches = getRepositoryService().getByMatchWeekId(matchWeekId);
+        return ResponseEntity.ok(new D11MatchesResponseBodyDTO()
+                .d11Matches(getMapper().map(d11Matches, D11MatchBaseDTO.class)));
     }
 
 }
