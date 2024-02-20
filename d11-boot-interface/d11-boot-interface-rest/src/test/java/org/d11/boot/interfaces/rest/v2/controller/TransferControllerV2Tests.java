d14238c1444d193093a980d204f79ab239d6ca23
dromelvan <dromelvan@aland.net>
2024-02-20 23:51:14 +0200

    Added tests for get transfers by player id API.

--- d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/TransferControllerV2Tests.java
+++ d11-boot-interface/d11-boot-interface-rest/src/test/java/org/d11/boot/interfaces/rest/v2/controller/TransferControllerV2Tests.java
@@ -5,8 +5,10 @@ import jakarta.transaction.Transactional;
 import org.d11.boot.api.v2.client.TransferApi;
 import org.d11.boot.api.v2.model.TransferDTO;
 import org.d11.boot.api.v2.model.TransfersResponseBodyDTO;
+import org.d11.boot.spring.model.Player;
 import org.d11.boot.spring.model.Transfer;
 import org.d11.boot.spring.model.TransferDay;
+import org.d11.boot.spring.repository.PlayerRepository;
 import org.d11.boot.spring.repository.TransferDayRepository;
 import org.d11.boot.spring.repository.TransferRepository;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -29,6 +32,12 @@ class TransferControllerV2Tests extends D11BootControllerV2Tests {
      */
     @Autowired
     private TransferDayRepository transferDayRepository;
+
+    /**
+     * Player repository.
+     */
+    @Autowired
+    private PlayerRepository playerRepository;
 
     /**
      * Transfer repository.
@@ -72,6 +81,46 @@ class TransferControllerV2Tests extends D11BootControllerV2Tests {
 
             assertEquals(map(transfers, TransferDTO.class), result,
                          "TransferControllerV2::getTransfersByTransferDayId equals");
+        }
+    }
+
+    /**
+     * Tests TransferControllerV2::getTransfersByPlayerId.
+     */
+    @Test
+    @Transactional
+    void testGetTransfersByPlayerId() {
+        final TransferApi transferApi = getApi(TransferApi.class);
+
+        assertThrows(FeignException.BadRequest.class,
+                () -> transferApi.getTransfersByPlayerId((Long) null),
+                "TransferControllerV2::getTransfersByPlayerId playerId null throws");
+
+        assertThrows(FeignException.BadRequest.class,
+                () -> transferApi.getTransfersByPlayerId(-1L),
+                "TransferControllerV2::getTransfersByPlayerId playerId negative throws");
+
+        final List<Transfer> ke = this.transferRepository.findAll();
+        final List<Player> players = this.playerRepository.findAll().stream()
+                .filter(player ->
+                        !this.transferRepository.findByPlayerIdOrderByTransferDayDatetimeDesc(player.getId()).isEmpty())
+                .toList();
+
+        assertFalse(players.isEmpty(), "TransferControllerV2::getTransfersByPlayerId players size > 0");
+
+        for (final Player player : players) {
+            final TransfersResponseBodyDTO transfersByPlayerId = transferApi.getTransfersByPlayerId(player.getId());
+            assertNotNull(transfersByPlayerId, "TransferControllerV2::getTransfersByPlayerId response not null");
+
+            final List<TransferDTO> result = transfersByPlayerId.getTransfers();
+
+            assertNotNull(result, "TransferControllerV2::getTransfersByPlayerId not null ");
+
+            final List<Transfer> transfers =
+                    this.transferRepository.findByPlayerIdOrderByTransferDayDatetimeDesc(player.getId());
+
+            assertEquals(map(transfers, TransferDTO.class), result,
+                    "TransferControllerV2::getTransfersByPlayerId equals");
         }
     }
 
