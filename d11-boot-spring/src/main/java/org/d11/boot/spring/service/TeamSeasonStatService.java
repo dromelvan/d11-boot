9e7645ef323113be715e4a7e873951934e6ad684
dromelvan <dromelvan@aland.net>
2024-08-22 18:22:10 +0300

    Added get player season stats by D11 team id and season id service method

--- d11-boot-spring/src/main/java/org/d11/boot/spring/service/TeamSeasonStatService.java
+++ d11-boot-spring/src/main/java/org/d11/boot/spring/service/TeamSeasonStatService.java
@@ -3,6 +3,7 @@ package org.d11.boot.spring.service;
 import org.d11.boot.spring.model.TeamSeasonStat;
 import org.d11.boot.spring.repository.TeamSeasonStatRepository;
 import org.d11.boot.util.exception.BadRequestException;
+import org.d11.boot.util.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,16 @@ import java.util.List;
  */
 @Service
 public class TeamSeasonStatService extends RepositoryService<TeamSeasonStat, TeamSeasonStatRepository> {
+
+    /**
+     * Season id property name.
+     */
+    private static final String SEASON_ID = "seasonId";
+
+    /**
+     * Must be positive error value.
+     */
+    private static final String MUST_BE_POSITIVE = "must be positive";
 
     /**
      * Creates a new team season stat service.
@@ -32,10 +43,29 @@ public class TeamSeasonStatService extends RepositoryService<TeamSeasonStat, Tea
      */
     public List<TeamSeasonStat> getBySeasonId(final Long seasonId) {
         if (seasonId == null || seasonId <= 0) {
-            throw new BadRequestException("seasonId", "must be positive");
+            throw new BadRequestException(SEASON_ID, MUST_BE_POSITIVE);
         }
 
         return getJpaRepository().findBySeasonIdOrderByRanking(seasonId);
+    }
+
+    /**
+     * Get team season stat by team id and season id.
+     *
+     * @param teamId   The team id.
+     * @param seasonId The season id.
+     * @return Team season stat by team id and season id.
+     */
+    public TeamSeasonStat getByTeamIdAndSeasonId(final Long teamId, final Long seasonId) {
+        if (teamId == null || teamId <= 0) {
+            throw new BadRequestException("teamId", MUST_BE_POSITIVE);
+        }
+        if (seasonId == null || seasonId <= 0) {
+            throw new BadRequestException(SEASON_ID, MUST_BE_POSITIVE);
+        }
+
+        return getJpaRepository().findByTeamIdAndSeasonId(teamId, seasonId)
+                .orElseThrow(() -> new NotFoundException(teamId, TeamSeasonStat.class));
     }
 
 }
--- d11-boot-spring/src/test/java/org/d11/boot/spring/service/TeamSeasonStatServiceTests.java
+++ d11-boot-spring/src/test/java/org/d11/boot/spring/service/TeamSeasonStatServiceTests.java
@@ -3,11 +3,13 @@ package org.d11.boot.spring.service;
 import org.d11.boot.spring.model.TeamSeasonStat;
 import org.d11.boot.spring.repository.TeamSeasonStatRepository;
 import org.d11.boot.util.exception.BadRequestException;
+import org.d11.boot.util.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -67,6 +69,68 @@ class TeamSeasonStatServiceTests extends BaseD11BootServiceTests {
         assertEquals(teamSeasonStats, result, "TeamSeasonStatService::getBySeasonId result equals");
 
         verify(this.teamSeasonStatRepository, times(1)).findBySeasonIdOrderByRanking(eq(seasonId));
+    }
+
+    /**
+     * Tests TeamSeasonStatService::getByTeamIdAndSeasonId.
+     */
+    @Test
+    void testGetByTeamIdAndSeasonId() {
+
+        // Validation --------------------------------------------------------------------------------------------------
+
+        final String teamIdProperty = "teamId";
+
+        final BadRequestException nullTeamIdException =
+                assertThrows(BadRequestException.class,
+                             () -> this.teamSeasonStatService.getByTeamIdAndSeasonId(null, 1L),
+                             "TeamSeasonStatService::getByTeamIdAndSeasonId null teamId throws");
+        assertEquals(teamIdProperty, nullTeamIdException.getParameter(),
+                     "TeamSeasonStatService::getByTeamIdAndSeasonId property equals null teamId");
+
+        final BadRequestException invalidTeamIdIdException =
+                assertThrows(BadRequestException.class,
+                             () -> this.teamSeasonStatService.getByTeamIdAndSeasonId(-1L, 1L),
+                             "TeamSeasonStatService::getByTeamIdAndSeasonId invalid teamId throws");
+        assertEquals(teamIdProperty, invalidTeamIdIdException.getParameter(),
+                     "TeamSeasonStatService::getByTeamIdAndSeasonId property equals invalid teamId");
+
+        final String seasonIdProperty = "seasonId";
+
+        final BadRequestException nullSeasonIdException =
+                assertThrows(BadRequestException.class,
+                             () -> this.teamSeasonStatService.getByTeamIdAndSeasonId(1L, null),
+                             "TeamSeasonStatService::getByTeamIdAndSeasonId null seasonId throws");
+        assertEquals(seasonIdProperty, nullSeasonIdException.getParameter(),
+                     "TeamSeasonStatService::getByTeamIdAndSeasonId property equals null seasonId");
+
+        final BadRequestException invalidSeasonIdException =
+                assertThrows(BadRequestException.class,
+                             () -> this.teamSeasonStatService.getByTeamIdAndSeasonId(1L, -1L),
+                             "TeamSeasonStatService::getByTeamIdAndSeasonId invalid seasonId throws");
+        assertEquals(seasonIdProperty, invalidSeasonIdException.getParameter(),
+                     "TeamSeasonStatService::getByTeamIdAndSeasonId property equals invalid seasonId");
+
+        // Success -----------------------------------------------------------------------------------------------------
+
+        final long teamId = 1L;
+        final long seasonId = 1L;
+        final TeamSeasonStat teamSeasonStat = generate(TeamSeasonStat.class);
+
+        when(this.teamSeasonStatRepository.findByTeamIdAndSeasonId(eq(teamId), eq(seasonId)))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class,
+                     () -> this.teamSeasonStatService.getByTeamIdAndSeasonId(teamId, seasonId));
+
+        when(this.teamSeasonStatRepository.findByTeamIdAndSeasonId(eq(teamId), eq(seasonId)))
+                .thenReturn(Optional.of(teamSeasonStat));
+
+        final TeamSeasonStat result = this.teamSeasonStatService.getByTeamIdAndSeasonId(teamId, seasonId);
+
+        assertEquals(teamSeasonStat, result, "TeamSeasonStatService::getByTeamIdAndSeasonId result equals");
+
+        verify(this.teamSeasonStatRepository, times(2)).findByTeamIdAndSeasonId(eq(teamId), eq(seasonId));
     }
 
 }
