fee49a78c50789c83944f0c95d8e12cbb7c9ed76
dromelvan <dromelvan@aland.net>
2024-08-24 17:16:07 +0300

    Modelmapper skips createdAt and updatedAt properties

--- d11-boot-util/src/main/java/org/d11/boot/util/mapper/modelmapper/ModelMapperMapper.java
+++ d11-boot-util/src/main/java/org/d11/boot/util/mapper/modelmapper/ModelMapperMapper.java
@@ -1,15 +1,32 @@
 package org.d11.boot.util.mapper.modelmapper;
 
 import org.d11.boot.util.mapper.Mapper;
+import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * Model mapper implementation of the Mapper interface.
  */
 public class ModelMapperMapper extends ModelMapper implements Mapper {
+
+    /**
+     * Set of names of properties that should not be mapped.
+     */
+    private static final Set<String> IGNORE_PROPERTIES = Set.of("createdAt", "updatedAt");
+
+    /**
+     * Creates a mapper and updates configuration.
+     */
+    public ModelMapperMapper() {
+        final Condition<Object, Object> condition =
+                context -> !IGNORE_PROPERTIES.contains(context.getMapping().getLastDestinationProperty().getName());
+
+        getConfiguration().setPropertyCondition(condition);
+    }
 
     @Override
     public <T> T map(final Object object, final Class<T> destinationClass) {
